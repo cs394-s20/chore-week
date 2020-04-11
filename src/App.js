a8b12e38f3253c3555f91891821a2fb4b43077d3
JiazheLiu <jiazheliu2021@u.northwestern.edu>
2020-04-11 17:30:30 -0500

    bottom navigation bar(v.20)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import GroupIcon from '@material-ui/icons/Group';
-import PersonIcon from '@material-ui/icons/Person';
-import InsertChartIcon from '@material-ui/icons/InsertChart';
-
-const useStyles = makeStyles({
-  root: {
-    width: 500,
-  },
-});
-
-export default function SimpleBottomNavigation() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
   return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction label="Groups" icon={<GroupIcon />} />
-      <BottomNavigationAction label="Me" icon={<PersonIcon />} />
-      <BottomNavigationAction label="Scoreboard" icon={<InsertChartIcon />} />
-    </BottomNavigation>
+      <div className="App-wrapper">
+        <Header/>
+        <div className="App-content"></div>
+        <Footer/>
+      </div>
   );
-}
   // return (
   //   <div>
   //     <header className="App-header">
@@ -68,6 +46,6 @@ export default function SimpleBottomNavigation() {
   //     </header>
   //   </div>
   // );
+}
 
-
-
+export default App;
