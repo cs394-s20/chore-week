098b0739477cf03d0565487202eac6f8a8862582
Noah Alvarado <np3474@gmail.com>
2020-04-15 15:27:41 -0500

    start on dialog

--- src/components/AddChore.js
+++ src/components/AddChore.js
@@ -1,8 +1,17 @@
 import React, {useState} from "react";
-import { Button, Dialog, DialogActions, DialogTitle, List, ListItem } from "@material-ui/core";
+import '../styles/AddChore.css';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, InputLabel, List, ListItem, MenuItem, Select, TextField } from "@material-ui/core";
+
+
+// TODO: Get groups from firebase
+const getGroups = () => ['personal'];
 
 const AddChore = () => {
     const [open, setOpen] = useState(false);
+
+    const [name, setName] = useState('New Chore');
+    const [dueDate, setDueDate] = useState(Date.now());
+    const [group, setGroup] = useState('personal');
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -12,24 +21,60 @@ const AddChore = () => {
         setOpen(false);
     };
 
+    const handleSave = () => {
+        const chore = {
+            name: name,
+            dueDate: dueDate,
+            group: group
+        };
+
+        console.log(chore);
+        setOpen(false);
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleGroupChange = (event) => {
+        setGroup(event.target.value);
+    };
+
     return (
         <div>
             <Button color="primary" onClick={handleClickOpen}>Add Chore</Button>
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle>Add a chore</DialogTitle>
-                <List>
-                    <ListItem>NameOfChore</ListItem>
-                    <ListItem>DueDateOfChore</ListItem>
-                    <ListItem>GroupOfChore</ListItem>
-                    <DialogActions>
-                        <Button onClick={handleClose} color="primary">
-                            Cancel
-                        </Button>
-                        <Button onClick={handleClose} color="primary">
-                            Subscribe
-                        </Button>
-                    </DialogActions>
-                </List>
+                <DialogContent className="dialog-root">
+                    <div className="dialog-wrap">
+                        <TextField id="chore-name"
+                                   label="Chore Name"
+                                   value={name}
+                                   onChange={handleNameChange}/>
+                        <div className="input-item">
+                            <InputLabel id="group-label">Group</InputLabel>
+                            <Select labelId="group-label"
+                                    id="chore-group"
+                                    value={group}
+                                    onChange={handleGroupChange}
+                            >
+                                {
+                                    getGroups().map(group => (
+                                        <MenuItem key={group} value={group}>{group}</MenuItem>
+                                    ))
+                                }
+                            </Select>
+                        </div>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSave} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
             </Dialog>
         </div>
     );
