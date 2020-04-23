441490d5f4d19fa339b88cf587e5fa3ea35f99a5
Noah Alvarado <np3474@gmail.com>
2020-04-23 16:28:09 -0500

    routing refactor

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React, {useState, useEffect} from 'react';
 import SignIn from "./components/SignIn/SignIn";
-import ChoresList from "./components/ChoresList";
+import { Link } from 'react-router-dom';
 import './App.css';
 import firebase from "./shared/firebase";
-import GroupsList from "./components/GroupsList"
-
-const pageSwitcher = (page, user) => {
-    switch (page){
-        case 0:
-            return (<ChoresList user={user}/>)
-        case 1:
-            return (<GroupsList uid={user.uid}/>)
-        default:
-            return
-    }
-}
 
 function App() {
-    const [page, setPage] = useState(1)
     const [user, setUser] = useState(firebase.auth().currentUser);
 
     useEffect(() => {
-        firebase.auth().onAuthStateChanged(setUser);
+        firebase.auth().onAuthStateChanged((user) => {
+            const elem = document.getElementById(user ? 'chores-link' : 'signin-link');
+            if (elem) {
+                setUser(user);
+                elem.click();
+            }
+        });
     }, []);
 
     return (
-        <div className="App-wrapper">
-            <Header/>
-            <div className="App-content">
-                {
-                    user ?
-                        pageSwitcher(page, user)
-                        :
-                        <SignIn uid={user ? user.uid : null}/>
-                }
-            </div>
-            <Footer tabswitch={setPage}/>
+        <div>
+            <Link id="signin-link" style={{ display: 'none'}} to="/" />
+            <Link id="chores-link" style={{ display: 'none'}} to="/chores" />
+            <SignIn uid={user ? user.uid : null}/>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link, BrowserRouter as Router} from 'react-router-dom';
 import '../styles/Footer.css';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
@@ -7,25 +8,35 @@ import PersonIcon from '@material-ui/icons/Person';
 import InsertChartIcon from '@material-ui/icons/InsertChart';
 
 
-export default function SimpleBottomNavigation(tabswitch) {
-  const [value, setValue] = React.useState(1);
-  const handleChange = (newValue) => {
-      tabswitch(newValue)
-  }
+export default function SimpleBottomNavigation() {
+    const [value, setValue] = React.useState(1);
 
-  return (
-    <BottomNavigation>
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-        handleChange(newValue);
-      }}
-      showLabels
-      className="footer-nav"
-    >
-      <BottomNavigationAction label="Groups" icon={<GroupIcon />} />
-      <BottomNavigationAction label="Me" icon={<PersonIcon />} />
-      <BottomNavigationAction style={{display: 'none'}} label="Scoreboard" icon={<InsertChartIcon />} disabled />
-    </BottomNavigation>
-  );
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            showLabels
+            className="footer-nav"
+        >
+                <Link to="/groups">
+                    <BottomNavigationAction label="Groups"
+                                            icon={<GroupIcon/>}/>
+                </Link>
+
+                <Link to="/chores">
+                    <BottomNavigationAction label="Me"
+                                            icon={<PersonIcon/>}/>
+                </Link>
+
+                <Link to="/scoreboard">
+                    <BottomNavigationAction style={{display: 'none'}}
+                                            label="Scoreboard"
+                                            icon={<InsertChartIcon/>}
+                                            disabled
+                    />
+                </Link>
+        </BottomNavigation>
+    );
 }
