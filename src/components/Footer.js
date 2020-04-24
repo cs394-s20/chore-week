f68ee58903d6f046b70177d6c3458d71dea2cdf9
JiazheLiu <jiazheliu2021@u.northwestern.edu>
2020-04-23 19:43:13 -0500

    fix foot navigation bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
+// import {Link, BrowserRouter as Router} from 'react-router-dom';
 import '../styles/Footer.css';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import GroupIcon from '@material-ui/icons/Group';
 import PersonIcon from '@material-ui/icons/Person';
 import InsertChartIcon from '@material-ui/icons/InsertChart';
+import { MemoryRouter as Router } from 'react-router';
+import { Link as RouterLink } from 'react-router-dom';
 
 
 export default function SimpleBottomNavigation() {
-    const [value, setValue] = React.useState(1);
+    const [value, setValue] = React.useState(0);
+    const LinkBehavior = React.forwardRef((props, ref) => (
+        <RouterLink ref={ref} to="/groups" {...props} />
+      ));
+
+    const LinkBehavior1 = React.forwardRef((props, ref) => (
+        <RouterLink ref={ref} to="/chores" {...props} />
+      ));
 
     return (
         <BottomNavigation
@@ -20,23 +29,23 @@ export default function SimpleBottomNavigation() {
             showLabels
             className="footer-nav"
         >
-                <Link to="/groups">
+                {/* <Link to="/groups"> */}
                     <BottomNavigationAction label="Groups"
-                                            icon={<GroupIcon/>}/>
-                </Link>
+                                            icon={<GroupIcon/>} component={LinkBehavior}/>
+                {/* </Link> */}
 
-                <Link to="/chores">
+                {/* <Link to="/chores"> */}
                     <BottomNavigationAction label="Me"
-                                            icon={<PersonIcon/>}/>
-                </Link>
+                                            icon={<PersonIcon/>} component={LinkBehavior1}/>
+                {/* </Link> */}
 
-                <Link to="/scoreboard">
+                {/* <Link to="/scoreboard"> */}
                     <BottomNavigationAction style={{display: 'none'}}
                                             label="Scoreboard"
                                             icon={<InsertChartIcon/>}
                                             disabled
                     />
-                </Link>
+                {/* </Link> */}
         </BottomNavigation>
     );
 }
