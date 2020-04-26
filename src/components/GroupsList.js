0f021ea91b29d5948c5472c744147fb94c7d3eeb
Noah Alvarado <np3474@gmail.com>
2020-04-26 16:01:07 -0500

    access group ids

--- src/components/GroupsList.js
+++ src/components/GroupsList.js
@@ -9,11 +9,13 @@ const db = firebase.database().ref();
 const addGroups = (uid, data) => {
     if (!uid) return;
 
-    const array = ['personal', ...Object.entries(data.groups).filter(
+    const array = [{gid: 'personal', name: 'personal'}, ...Object.entries(data.groups).filter(
         ([gid, group]) => {
             return(Object.values(group)[0].includes(uid))
         }
-    ).map(([gid, group]) => Object.keys(group)[0])]
+    ).map(([gid, group]) => {
+        return { gid, name: Object.keys(group)[0]};
+    })]
 
     return(array)
 }
@@ -48,7 +50,7 @@ const GroupsList = () => {
             <Grid container justify="center" align-items="flex-start" spacing={3}>
                 <React.Fragment>
                     {groups.map((group) => {
-                        return (<Group group={group} key={group}/>)
+                        return (<Group group={group} key={group.gid}/>)
                     })}
                 </React.Fragment>
             </Grid>
@@ -61,7 +63,7 @@ const Group = ({group}) => {
     return (
         <Grid item xs={6}>
             <Paper className='card'>
-                {group}
+                {group.gid} | {group.name}
             </Paper>
         </Grid>
     )
